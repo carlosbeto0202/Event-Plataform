245f56375f4077f9af497b5e68e7ab0a6b0d4656
Beto <carlosbeto0202@gmail.com>
2022-06-26 12:36:29 -0300

    fim 3 aula

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,16 @@
-import { gql,useQuery } from "@apollo/client"
-import { useEffect } from "react"
+import { ApolloProvider } from "@apollo/client"
+import { BrowserRouter } from "react-router-dom"
+import { client } from "./lib/apollo"
+import { Router } from "./Router"
 
-const GET_LESSONS_QUERY = gql`
-  query{
-    lessons{
-      id
-      title
-    }
-  }
-`
 
-interface Lesson{
-  id:string;
-  title: string
-}
-/* Forma de escrever uma Query */
-/* function App(){ */
-
- /*  useEffect(() => {
-    client.query({
-      query:GET_LESSONS_QUERY,
-    }).then(response=>{
-      console.log(response.data)
-    })
-  }, []) */
-  
 function App(){
-
-  const{ data } = useQuery<{ lessons:Lesson[] } >(GET_LESSONS_QUERY)
-
-  return (
-     <ul> 
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-     </ul>
+  return(
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
